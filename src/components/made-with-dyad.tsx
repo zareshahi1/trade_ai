e0e6519f58fbdcdbf55a53b1e9c34a384f67c121
[dyad] <satreyek@gmail.com>
2025-10-26 16:18:54 +0330

    developed By Ali Jahani

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -2,12 +2,12 @@ export const MadeWithDyad = () => {
   return (
     <div className="p-4 text-center">
       <a
-        href="https://www.dyad.sh/"
+        href="https://jahaniwww.com"
         target="_blank"
         rel="noopener noreferrer"
         className="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
       >
-        Made with Dyad
+        Developed By : Ali Jahani
       </a>
     </div>
   );
